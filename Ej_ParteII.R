0cbfd2bf6e4395e2c7e972d2514fa033f45ef4f2
Elizabeth_Ramos <jacquelineeee115@gmail.com>
2021-07-02 23:52:16 -0500

    Primera versión

---
+++ Ej_ParteII.R
@@ -0,0 +1,13 @@
+### PARTE 2
+
+# 1  Graficar los puntos * en un plano
+
+
+
+
+# 2. Ingresar la matriz A 
+vector01 <- seq(1, 4)
+vector02 <- seq(2, 8, by = 2)
+vector03 <- seq(3, 12, by = 3)
+dim(vector01) <- c(4, 3)
+matrix01 <- cbind(vector01, vector02, vector03)
